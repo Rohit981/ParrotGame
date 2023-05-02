65a1cccf8e0a8366349629fa1a4ae122d919f42d
Rohit <rohitpathak987@gmail.com>
2023-05-01 21:57:55 -0700

    First Commit

---
+++ SeaAdventure/Intermediate/Build/Win64/UnrealEditor/Inc/SeaAdventure/UHT/SeaAdventureGameModeBase.generated.h
@@ -0,0 +1,98 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "SeaAdventureGameModeBase.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SEAADVENTURE_SeaAdventureGameModeBase_generated_h
+#error "SeaAdventureGameModeBase.generated.h already included, missing '#pragma once' in SeaAdventureGameModeBase.h"
+#endif
+#define SEAADVENTURE_SeaAdventureGameModeBase_generated_h
+
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_SPARSE_DATA
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_RPC_WRAPPERS
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_ACCESSORS
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASeaAdventureGameModeBase(); \
+	friend struct Z_Construct_UClass_ASeaAdventureGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASeaAdventureGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SeaAdventure"), NO_API) \
+	DECLARE_SERIALIZER(ASeaAdventureGameModeBase)
+
+
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASeaAdventureGameModeBase(); \
+	friend struct Z_Construct_UClass_ASeaAdventureGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASeaAdventureGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SeaAdventure"), NO_API) \
+	DECLARE_SERIALIZER(ASeaAdventureGameModeBase)
+
+
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASeaAdventureGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASeaAdventureGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASeaAdventureGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASeaAdventureGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASeaAdventureGameModeBase(ASeaAdventureGameModeBase&&); \
+	NO_API ASeaAdventureGameModeBase(const ASeaAdventureGameModeBase&); \
+public: \
+	NO_API virtual ~ASeaAdventureGameModeBase();
+
+
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASeaAdventureGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASeaAdventureGameModeBase(ASeaAdventureGameModeBase&&); \
+	NO_API ASeaAdventureGameModeBase(const ASeaAdventureGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASeaAdventureGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASeaAdventureGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASeaAdventureGameModeBase) \
+	NO_API virtual ~ASeaAdventureGameModeBase();
+
+
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_12_PROLOG
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_SPARSE_DATA \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_RPC_WRAPPERS \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_ACCESSORS \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_INCLASS \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_SPARSE_DATA \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_ACCESSORS \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SEAADVENTURE_API UClass* StaticClass<class ASeaAdventureGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_SeaAdventure_Source_SeaAdventure_SeaAdventureGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ SeaAdventure/Source/SeaAdventure/SeaAdventureGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SeaAdventureGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEAADVENTURE_API ASeaAdventureGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
