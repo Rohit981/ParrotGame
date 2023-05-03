d1199738a196c0a957843b36dba56635756f6fc2
ccd729 <funbaba@live.com>
2023-05-02 20:24:55 -0700

    Imported character mesh. Implemented C++ movement with jump (forward axis)

--- SeaAdventure/Intermediate/Build/Win64/UnrealEditor/Inc/SeaAdventure/UHT/SeaAdventure.init.gen.cpp
+++ SeaAdventure/Intermediate/Build/Win64/UnrealEditor/Inc/SeaAdventure/UHT/SeaAdventure.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeSeaAdventure_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xEC814D0A,
-				0x0B139E4C,
+				0x66A03063,
+				0x6E3C4910,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_SeaAdventure.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_SeaAdventure.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SeaAdventure(Z_Construct_UPackage__Script_SeaAdventure, TEXT("/Script/SeaAdventure"), Z_Registration_Info_UPackage__Script_SeaAdventure, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xEC814D0A, 0x0B139E4C));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SeaAdventure(Z_Construct_UPackage__Script_SeaAdventure, TEXT("/Script/SeaAdventure"), Z_Registration_Info_UPackage__Script_SeaAdventure, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x66A03063, 0x6E3C4910));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
